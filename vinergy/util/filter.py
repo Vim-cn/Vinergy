e615c6c33db7633ce21f95f8898a055859857bcd
Vayn <vayn@vayn.de>
2011-07-30 21:49:13 +0800

    improve custom filter

--- vinergy/util/filter.py
+++ vinergy/util/filter.py
@@ -10,6 +10,8 @@
 
   Custom Pygments Filter
 '''
+import re
+
 from pygments.util import get_int_opt
 from pygments.filter import Filter
 
@@ -17,9 +19,15 @@ class TabFilter(Filter):
   def __init__(self, **options):
     Filter.__init__(self, **options)
     self.tabsize = get_int_opt(options, 'tabsize', 4)
+    self.repl = re.compile(r'^\t+')
+
+  def sub(self, tabs):
+    def substitution(match):
+      return match.group(0).replace('\t', tabs)
+    return substitution
 
   def filter(self, lexer, stream):
     tabs = ' ' * self.tabsize
     for ttype, value in stream:
-      value = value.replace('\t', tabs)
+      value = self.repl.sub(self.sub(tabs), value).expandtabs(1)
       yield ttype, value
