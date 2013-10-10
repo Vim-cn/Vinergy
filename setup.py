abb19f72ecdfcc4508581a3ec180baa06a0ab3d2
lilydjwg <lilydjwg@gmail.com>
2013-10-10 23:03:06 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
   download_url = 'https://github.com/Vim-cn/vinergy',
   license = 'GPL3',
   install_requires = ['tornado>=3.1', 'pymongo', 'pygments'],
-  packages = find_packages,
+  packages = find_packages(),
   classifiers = [
     'Development Status :: 4 - Beta',
     'Environment :: Web Environment',
