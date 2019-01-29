1acc42e6eb6950479be20c411ea86d81da525390
lilydjwg <lilydjwg@gmail.com>
2019-01-29 21:29:25 +0800

    use a connection pool for db connection

--- vinergy/model.py
+++ vinergy/model.py
@@ -5,28 +5,30 @@ import asyncpg
 
 from .util.b52 import b52_encode
 
-DB_CONN = None
+DB_POOL = None
 
 async def setup(db):
-  global DB_CONN
-  DB_CONN = await asyncpg.connect(db)
+  global DB_POOL
+  DB_POOL = await asyncpg.create_pool(db)
 
 async def get_code_by_name(name, syntax):
-  row = await DB_CONN.fetchrow(
-    '''update rendered_code set visited_at = $1
-       where name = $2 and syntax = $3
-       returning content
-    ''', datetime.now(), name, syntax,
-  )
+  async with DB_POOL.acquire() as conn:
+    row = await conn.fetchrow(
+      '''update rendered_code set visited_at = $1
+        where name = $2 and syntax = $3
+        returning content
+      ''', datetime.now(), name, syntax,
+    )
   if row is None:
     raise FileNotFoundError
   return row['content']
 
 async def get_raw_code_by_name(name):
-  row = await DB_CONN.fetchrow(
-    '''update raw_code set visited_at = $1 where name = $2
-       returning content
-    ''', datetime.now(), name)
+  async with DB_POOL.acquire() as conn:
+    row = await conn.fetchrow(
+      '''update raw_code set visited_at = $1 where name = $2
+        returning content
+      ''', datetime.now(), name)
   if row is None:
     raise FileNotFoundError
   return row['content']
@@ -34,9 +36,10 @@ async def get_raw_code_by_name(name):
 async def get_codename_by_content(content):
   h = hashlib.sha1(content.encode('utf-8'))
   sha1sum = h.digest()
-  row = await DB_CONN.fetchrow(
-    '''select name from raw_code
-       where sha1sum = $1''', sha1sum)
+  async with DB_POOL.acquire() as conn:
+    row = await conn.fetchrow(
+      '''select name from raw_code
+        where sha1sum = $1''', sha1sum)
   if row is None:
     raise FileNotFoundError
   return row['name']
@@ -44,23 +47,26 @@ async def get_codename_by_content(content):
 async def insert_code(content):
   h = hashlib.sha1(content.encode('utf-8'))
   sha1sum = h.digest()
-  row = await DB_CONN.fetchrow(
-    '''insert into raw_code
-       (content, sha1sum) values
-       ($1, $2) returning id
-    ''', content, sha1sum)
+  async with DB_POOL.acquire() as conn:
+    async with conn.transaction():
+      row = await conn.fetchrow(
+        '''insert into raw_code
+          (content, sha1sum) values
+          ($1, $2) returning id
+        ''', content, sha1sum)
 
-  name = b52_encode(row['id'])
-  await DB_CONN.execute(
-    '''update raw_code
-       set name = $1 where id = $2''',
-    name, row['id'])
+      name = b52_encode(row['id'])
+      await conn.execute(
+        '''update raw_code
+          set name = $1 where id = $2''',
+        name, row['id'])
 
   return name
 
 async def update_code(name, content, syntax):
-  await DB_CONN.execute(
-    '''insert into rendered_code
-       (name, content, syntax) values
-       ($1, $2, $3)''',
-    name, content, syntax)
+  async with DB_POOL.acquire() as conn:
+    await conn.execute(
+      '''insert into rendered_code
+        (name, content, syntax) values
+        ($1, $2, $3)''',
+      name, content, syntax)
